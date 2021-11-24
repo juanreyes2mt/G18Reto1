45fdea0797fe0b79913a95aaead81a9e3e9acae1
Santiago Pérez <santiago.perez.mt@correo.usa.edu.co>
2021-11-24 17:43:05 -0500

    Agregados comentarios a las funciones para buenas prácticas

--- src/main/java/com/cuatroa/retouno/controller/UserController.java
+++ src/main/java/com/cuatroa/retouno/controller/UserController.java
@@ -16,31 +16,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
- * @author desarrolloextremo
+ * @author 
  */
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
 public class UserController {
+    /**
+     * 
+     */
     @Autowired
     private UserService userService;
-    
+    /**
+     * 
+     * @return 
+     */
     @GetMapping("/all")
     public List<User> getAll() {
         return userService.getAll();
     }
-    
+    /**
+     * 
+     * @param user
+     * @return 
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User registrar(@RequestBody User user) {
         return userService.registrar(user);
     }
-    
+    /**
+     * 
+     * @param email
+     * @param password
+     * @return 
+     */
     @GetMapping("/{email}/{password}")
     public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password) {
         return userService.autenticarUsuario(email, password);
     }
-    
+    /**
+     * 
+     * @param email
+     * @return 
+     */
     @GetMapping("/{email}")
     public boolean existeEmail(@PathVariable("email") String email) {
         return userService.existeEmail(email);
